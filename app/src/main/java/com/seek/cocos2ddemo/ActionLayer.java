2e152ad1a85da0bfb8717c57742d7d9f7a81d7e1
zhangjunling4 <15637181917@163.com>
2017-09-08 09:28:53 +0800

    颜色渐变的效果 复杂动作的实现

--- app/src/main/java/com/seek/cocos2ddemo/ActionLayer.java
+++ app/src/main/java/com/seek/cocos2ddemo/ActionLayer.java
@@ -4,10 +4,15 @@ import org.cocos2d.actions.base.CCRepeatForever;
 import org.cocos2d.actions.interval.CCJumpBy;
 import org.cocos2d.actions.interval.CCMoveBy;
 import org.cocos2d.actions.interval.CCMoveTo;
+import org.cocos2d.actions.interval.CCRotateBy;
 import org.cocos2d.actions.interval.CCSequence;
+import org.cocos2d.actions.interval.CCSpawn;
+import org.cocos2d.actions.interval.CCTintBy;
 import org.cocos2d.layers.CCLayer;
+import org.cocos2d.nodes.CCLabel;
 import org.cocos2d.nodes.CCNode;
 import org.cocos2d.nodes.CCSprite;
+import org.cocos2d.types.ccColor3B;
 
 /**
  * Created by admin on 2017/9/7.
@@ -24,8 +29,30 @@ public class ActionLayer extends CCLayer {
 
 //        moveTo(ccSprite);
 //        moveBy(ccSprite);
-        jumpBy(ccSprite);
+        jumpBy2(ccSprite);
 
+        tint();
+
+    }
+
+    /**
+     * 颜色渐变的效果
+     * CCLabel 专门用于显示文字的精灵
+     */
+    private void tint() {
+        CCLabel label = CCLabel.makeLabel("那些年，我们一起玩坏的僵尸...", "hkbd.ttf", 24);
+        label.setColor(ccc3(50,0,255));
+        label.setAnchorPoint(0, 0);
+        label.setPosition(100, 50);
+        this.addChild(label);
+
+        ccColor3B ccColor3B = ccc3(100, 255, -100);
+        CCTintBy by = CCTintBy.action(1, ccColor3B);
+        CCTintBy reverse = by.reverse();
+        CCSequence sequence = CCSequence.actions(by, reverse);
+        CCRepeatForever forever = CCRepeatForever.action(sequence);
+
+        label.runAction(forever);
     }
 
     private void jumpBy(CCSprite ccSprite) {
@@ -35,6 +62,19 @@ public class ActionLayer extends CCLayer {
         CCRepeatForever forever = CCRepeatForever.action(ccSequence);
         ccSprite.
         runAction(forever);
+    }
+
+    private void jumpBy2(CCSprite ccSprite) {
+        CCJumpBy ccJumpBy = CCJumpBy.action(4, ccp(100, 100), 100, 2);
+        CCRotateBy ccRotateBy = CCRotateBy.action(2, 360);
+
+        CCSpawn ccSpawn = CCSpawn.actions(ccJumpBy, ccRotateBy);
+//        CCJumpBy reverse = ccJumpBy.reverse();
+        CCSequence ccSequence = CCSequence.actions(ccSpawn, ccSpawn.reverse());
+        CCRepeatForever forever = CCRepeatForever.action(ccSequence);
+        ccSprite.setAnchorPoint(0.5f, 0.5f);
+        ccSprite.setPosition(50, 50);
+        ccSprite.runAction(forever);
     }
 
     private void moveBy(CCSprite ccSprite) {
